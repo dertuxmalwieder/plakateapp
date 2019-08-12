879f911557fdc2a00a5fefe17adf8d4b70d1939f
git@tuxproject.de <git@tuxproject.de@noemail.net>
2019-08-12 19:17:57 +0000

    Wir haben jetzt eine Straßensuche!
    FossilOrigin-Name: 7d0c6ad749073c1a99b31731b76714caacffda6f3f987a9159ee393ad906d400

--- src/plakateapp.go
+++ src/plakateapp.go
@@ -2,11 +2,12 @@ package main
 
 import (
     "encoding/json"
+    "encoding/xml"
     "fmt"
     "html/template"
+    "io/ioutil"
     "log"
     "net/http"
-    "strconv"
 
     // Bibliotheken aus GitHub:
     _ "github.com/mattn/go-sqlite3"
@@ -19,42 +20,68 @@ type Plakat struct {
     ID          int     `db:"id"`
     Latitude    float32 `db:"lat"`
     Longitude   float32 `db:"lon"`
+    Location    string  `db:"location"`
 }
 
+// Reverse-Geocoding via Nominatim:
+
+type ReverseGeoCode struct {
+    XMLName     xml.Name    `xml:"reversegeocode"`
+    AdressParts AdressParts `xml:"addressparts"`
+}
+
+type AdressParts struct {
+    XMLName      xml.Name   `xml:"addressparts"`
+    HouseNumber  string     `xml:"house_number"`
+    Road         string     `xml:"road"`
+    Suburb       string     `xml:"suburb"`
+    District     string     `xml:"city_district"`
+    City         string     `xml:"city"`
+    State        string     `xml:"state"`
+    Postcode     string     `xml:"postcode"`
+    Country      string     `xml:"country"`
+    CountryCode  string     `xml:"country_code"`
+}
+
+
+// ----------------------------------------
+
 func CheckError(err error) {
-    // Bei Fehlern schreiend im Kreis rennen:
     if err != nil {
         panic(err)
     }
 }
 
+func GetXML(url string) ([]byte, error) {
+    // XML herunterladen:
+    resp, err := http.Get(url)
+    CheckError(err)
+    defer resp.Body.Close()
+
+    if resp.StatusCode != http.StatusOK {
+        return []byte{}, fmt.Errorf("Statusfehler: %v", resp.StatusCode)
+    }
+
+    data, err := ioutil.ReadAll(resp.Body)
+    CheckError(err)
+
+    return data, nil
+}
+
 func FetchPlakate() []Plakat {
-    // Liste von Plakaten aus der DB in ein Plakat-Array schieben:
     db, err := sqlx.Open("sqlite3", "./plakate.db")
     CheckError(err)
     defer db.Close()
-    
+
     // Liste erzeugen:
     plakate := []Plakat{}
     db.Select(&plakate, "SELECT * FROM plakate")
-    
+
     return plakate
 }
 
-func DeletePlakat(id string) {
-    // Datenbank aufrufen:
-    db, err := sqlx.Open("sqlite3", "./plakate.db")
-    CheckError(err)
-    defer db.Close()
-    
-    // Löschen, falls möglich:
-    stmt, err := db.Prepare("delete from plakate where id = ?")
-    _, err = stmt.Exec(strconv.Atoi(id))
-    CheckError(err)
-}
-    
 // ----------------------------------------
-   
+
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
     // Startseite aufrufen:
     tmpl := template.Must(template.ParseFiles("templates/index.htm"))
@@ -64,7 +91,7 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 func ManagePlakateHandler(w http.ResponseWriter, r *http.Request) {
     // Datenbank aufrufen:
     plakate := FetchPlakate()
-    
+
     // delete.htm mit Plakate-struct aufrufen:
     tmpl := template.Must(template.ParseFiles("templates/delete.htm"))
     tmpl.Execute(w, plakate)
@@ -73,7 +100,7 @@ func ManagePlakateHandler(w http.ResponseWriter, r *http.Request) {
 func ListPlakateHandler(w http.ResponseWriter, r *http.Request) {
     // Datenbank aufrufen:
     plakate := FetchPlakate()
-    
+
     // JSON-Objekt ausgeben:
     jsonobj, err := json.Marshal(plakate)
     CheckError(err)
@@ -84,33 +111,81 @@ func NeuesPlakatHandler(w http.ResponseWriter, r *http.Request) {
     // Plakat mit "lat" und "lon" erzeugen:
     lat := r.FormValue("lat")
     lon := r.FormValue("lon")
-    
+
+    // Geocoding:
+    url := fmt.Sprintf("https://nominatim.openstreetmap.org/reverse?lat=%s&lon=%s", lat, lon)
+    xmlBytes, err := GetXML(url)
+    CheckError(err)
+
+    var xmlFile ReverseGeoCode
+    xml.Unmarshal(xmlBytes, &xmlFile)
+
+    adressData := xmlFile.AdressParts
+
+    p1 := ""
+    p2 := ""
+    p3 := ""
+
+    if adressData.Road != "" {
+        p1 = fmt.Sprintf("%s %s, ", adressData.Road, adressData.HouseNumber)
+    }
+
+    if adressData.District != "" {
+        p2 = fmt.Sprintf("%s, ", adressData.District)
+    }
+
+    if adressData.Postcode != "" {
+        p3 = fmt.Sprintf("%s %s", adressData.Postcode, adressData.City)
+    }
+
+    location := fmt.Sprintf("%s%s%s", p1, p2, p3)
+
     // Datenbank aufrufen:
     db, err := sqlx.Open("sqlite3", "./plakate.db")
     CheckError(err)
     defer db.Close()
-    
-    stmt, err := db.Prepare("insert into plakate (lat, lon) values (?, ?)")
-    _, err = stmt.Exec(lat, lon)
+
+    stmt, err := db.Prepare("insert into plakate (lat, lon, location) values (?, ?, ?)")
     CheckError(err)
-    
+    _, err = stmt.Exec(lat, lon, location)
+    CheckError(err)
+
     fmt.Fprintf(w, "Plakat erfolgreich eingetragen!")
 }
 
 func DelHandler(w http.ResponseWriter, r *http.Request) {
-    // Plakat mit URL-Parameter "id" löschen:
+    // Plakat mit vars["id"] löschen:
     vars := mux.Vars(r)
-    DeletePlakat(vars["id"])
-    
+
+    // Datenbank aufrufen:
+    db, err := sqlx.Open("sqlite3", "./plakate.db")
+    CheckError(err)
+    defer db.Close()
+
+    // Löschen, falls möglich:
+    stmt, err := db.Prepare("delete from plakate where id = ?")
+    CheckError(err)
+    _, err = stmt.Exec(vars["id"])
+    CheckError(err)
+
     // Falls kein Fehler aufgetreten ist, umleiten auf /manageplakate:
     http.Redirect(w, r, "/manageplakate", http.StatusMovedPermanently)
 }
 
 func DelPostHandler(w http.ResponseWriter, r *http.Request) {
-    // Plakat mit POST-Parameter "id" löschen:
     data := r.FormValue("id")
-    DeletePlakat(data)
-    
+
+    // Datenbank aufrufen:
+    db, err := sqlx.Open("sqlite3", "./plakate.db")
+    CheckError(err)
+    defer db.Close()
+
+    // Löschen, falls möglich:
+    stmt, err := db.Prepare("delete from plakate where id = ?")
+    CheckError(err)
+    _, err = stmt.Exec(data)
+    CheckError(err)
+
     // Falls kein Fehler aufgetreten ist, umleiten auf /manageplakate:
     http.Redirect(w, r, "/manageplakate", http.StatusMovedPermanently)
 }
@@ -124,7 +199,7 @@ func main() {
     r.HandleFunc("/neuesplakat", NeuesPlakatHandler).Methods("POST")
     r.HandleFunc("/del/{id:[0-9]+}", DelHandler)
     r.HandleFunc("/delpost", DelPostHandler).Methods("POST")
-    
+
     // script.js auch aus /templates ausliefern:
     r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./templates"))))
     http.Handle("/", r)
